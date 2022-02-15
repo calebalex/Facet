de0c31ab1753a0fb6df0140311490589bf2d8a8e
calebalex <caleb@sabado.com>
2022-02-15 13:49:33 -0500

    Component File Rename

--- src/componentIndex.js
+++ src/componentIndex.js
@@ -1,3 +1,3 @@
-export {default as LoginComponent} from './ScreenComponents/LoginComponent.js';
-export {default as RegisterComponent} from './ScreenComponents/RegisterComponent.js';
-export {default as EntryList} from './ScreenComponents/EntryList.js';
+export {default as LoginComponent} from './Components/LoginComponent.js';
+export {default as RegisterComponent} from './Components/RegisterComponent.js';
+export {default as EntryList} from './Components/EntryList.js';
